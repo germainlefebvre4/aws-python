7731969275ab7640d27f4b4eda320391528ce734
Germain <7525033+germainlefebvre4@users.noreply.github.com>
2018-03-23 17:53:31 +0100

    Update reset_ec2.py

--- reset_ec2.py
+++ reset_ec2.py
@@ -4,40 +4,41 @@ import boto3
 
 region = "eu-central-1"
 
+print("- List all Applicaiton Autoscaling")
+#appautoscalingClient = boto3.client('application-autoscaling', region_name=region)
+#appautoscaling = boto3.resource('application-autoscaling', region_name=region)
 
-ec2client = boto3.client('ec2', region_name=region)
+print("- List all Autoscaling")
+#autoscalingClient = boto3.client('autoscaling', region_name=region)
+
+
+print("- List all ELB")
+elbClient = boto3.client('elb', region_name=region)
+bals = elbClient.describe_load_balancers()
+if len(bals["LoadBalancerDescriptions"]) > 0:
+  for elb in bals["LoadBalancerDescriptions"]:
+    print elb['DNSName']
+
+print("- List all ELB v2")
+elbv2Client = boto3.client('elbv2', region_name=region)
+bals = elbv2Client.describe_load_balancers()
+if len(bals["LoadBalancers"]) > 0:
+  for elb in bals["LoadBalancers"]:
+    print elb['DNSName']
+
+print("- List all EC2")
+ec2Client = boto3.client('ec2', region_name=region)
 ec2 = boto3.resource('ec2', region_name=region)
+for instance in ec2.instances.all():
+  print instance.id
 
-print("-----")
-
-# Working
-#services = [a for a in dir(ec2client) if 'describe' in a ]
-#print "\n".join(services)
-#service = services[41]
-#print service
-#exec("entity = ec2client." + service + "()")
-#print entity
-
-# During
-services = ["instances","internet_gateways","key_pairs","network_acls","network_interfaces","placement_groups","route_tables","security_groups","subnets","volumes","vpcs"]
-service = services[0]
-for service in services:
-  print("- %s" % (service))
-  exec("entities = ec2." + service + ".all()")
-  for entity in entities:
-    #print dir(entity)
-    #if 'id' in entity.keys():
-    if hasattr(entity,'id'):
-      print entity.id
-    elif hasattr(entity,'name'):
-      print entity.name
-
-print("------")
+print("- List all Subnets")
 subnets = ec2client.describe_subnets()
-dir(subnets)
 for subnet in subnets['Subnets']:
   print subnet['SubnetId']
 
+print("- List all VPCs")
 vpcs = ec2client.describe_vpcs()
 for vpc in vpcs['Vpcs']:
   print vpc['VpcId']
+
